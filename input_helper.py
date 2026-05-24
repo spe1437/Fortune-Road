082ed193f92e32f8af0ef7c9bb6b50078cc8cccd
Yuchen Jiang <158550373+spe1437@users.noreply.github.com>
2026-05-24 16:09:59 +0800

    Update input_helper.py

--- input_helper.py
+++ input_helper.py
@@ -1,6 +1,12 @@
-def get_choice(prompt: str, valid_choice: list[str]) -> str:
+def get_choice(prompt: str, valid_choices: list[str]) -> str:
+    '''
+    Repeatedly asks the user for input until
+    a valid choice is entered.
+
+    Returns the validated user choice.
+    '''
     while True:
         choice = input(prompt)
-        if choice in valid_choice:
+        if choice in valid_choices:
             return choice
         print("Invalid choice. Please try again.")
